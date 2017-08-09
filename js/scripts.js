429d9151d274f5d1b4a271ad2a12c08d42a925a3
Elrey Belmonti and Kristen Kulha <elrey@>
2017-08-09 16:07:40 -0700

    Add jQuery selectors to get user input and display.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $("#store-form").submit(function(){
+    var nameInput = $("input#name").val();
+    var streetInput = $("input#street").val();
+    var cityInput = $("input#city").val();
+    var stateInput = $("input#state").val();
+    var zipInput = $("input#zip").val();
+    var jewelryInput = $("select#jewelry").val();
+    var stoneInput = $("input:radio[name=stone]:checked").val();
+
+    $(".name").text(nameInput);
+    $(".street").text(streetInput);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
+    $(".zip").text(zipInput);
+    $(".jewelry").text(jewelryInput);
+    $(".stone").text(stoneInput);
+
+    $("#store-form").hide();
+    $(".receipt-page").show();
+    event.preventDefault();
+
+  });
+});
